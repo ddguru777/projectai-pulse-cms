7ef8979c7dc946d670584d3e164efa35af876e32
devguru <ddguru777@gmail.com>
2019-10-18 03:11:36 -0700

    customize page

--- page_setting/admin.py
+++ page_setting/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from .models import PageSetting
+from .models import PageSetting, ExtendedPage
 from aboutme.models import AMQuestion
 from aboutothers.models import AOQuestion
+from cms.admin.pageadmin import PageAdmin
+from cms.models.pagemodel import Page
+from jet.admin import CompactInline
 
-class AMQuestionInline(admin.StackedInline):
+class ExtendedPageAdmin(admin.StackedInline):
+    model = ExtendedPage
+    can_delete = False
+
+PageAdmin.inlines.append(ExtendedPageAdmin)
+try:
+    admin.site.unregister(Page)
+except:
+    pass
+admin.site.register(Page, PageAdmin)
+
+class AMQuestionInline(CompactInline):
     model = AMQuestion
     extra = 0
 
-class AOQuestionInline(admin.StackedInline):
+class AOQuestionInline(CompactInline):
     model = AOQuestion
     extra = 0
 
@@ -17,5 +31,7 @@ class PageSettingAdmin(admin.ModelAdmin):
         (None,      {'fields': ['page', 'pageType']}),
     ]
     inlines = [AMQuestionInline, AOQuestionInline]
+
+    list_display = ('page', 'pageType')
 
 admin.site.register(PageSetting, PageSettingAdmin)
--- page_setting/models.py
+++ page_setting/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cms.models import Page
+from cms.models.pagemodel import Page
 from enumfields import EnumField
 from enumfields import Enum
 
@@ -12,16 +12,19 @@ class PageType(Enum):
     PG_ABOUT_STAKEHOLDER = 5
     PG_RESULT = 6
     
-    # class Labels:
-    #     PG_WELCOME1 = 'Welcome 1'
-    #     PG_WELCOME2 = 'Welcome 2'
-    #     PG_ABOUT_ME = 'About Me'
-    #     PG_SURVEY = 'Survey'
-    #     PG_NEW_STAKEHOLDER = 'New StakeHolder'
-    #     PG_ABOUT_STAKEHOLDER = 'About StakeHolder'
-    #     PG_RESULT = 'Result'
+    class Labels:
+        PG_WELCOME1 = 'Welcome 1'
+        PG_WELCOME2 = 'Welcome 2'
+        PG_ABOUT_ME = 'About Me'
+        PG_SURVEY = 'Survey'
+        PG_NEW_STAKEHOLDER = 'New StakeHolder'
+        PG_ABOUT_STAKEHOLDER = 'About StakeHolder'
+        PG_RESULT = 'Result'
 
 class PageSetting(models.Model):
     page = models.OneToOneField(Page, on_delete=models.CASCADE, primary_key=True)
     pageType = EnumField(PageType, max_length=1)
 
+class ExtendedPage(models.Model):
+    page = models.ForeignKey(Page, unique=True, verbose_name="Page", editable=False, related_name='extended_fields')
+    my_extra_field = models.CharField(max_length=50)
